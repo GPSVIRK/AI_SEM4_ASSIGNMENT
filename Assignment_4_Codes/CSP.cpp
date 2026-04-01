1b4047ff5fbecd1bd41255df1f2384bdd87ea20c
GPSVIRK <gpsvirk1@gmail.com>
2026-04-01 11:32:28 +0530

    csv parser added

---
+++ Assignment_4_Codes/CSP.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#include "state_district_connections.hpp"
+
+using namespace std;
+
+int main(){
+    StateDistrictConnections* aus = new StateDistrictConnections("Australia.csv");
+    StateDistrictConnections* tel = new StateDistrictConnections("Telangana.csv");
+}
---
+++ Assignment_4_Codes/state_district_connections.hpp
@@ -0,0 +1,71 @@
+#ifndef STATEDISTRICT_H
+#define STATEDISTRICT_H
+
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <fstream>
+#include <ios>
+#include <stdexcept>
+
+using namespace std;
+
+class StateDistrictConnections{
+    public:
+    vector<string> names;
+    StateDistrictConnections(string fileName){
+        fstream fin;
+
+        fin.open(fileName,ios::in);
+        if(!fin.is_open()) throw runtime_error("Failed to open csv file");
+
+        string line, word;
+
+        while(getline(fin, line)){
+            stringstream s(line);
+
+            getline(s,word,',');
+
+            this->names.push_back(word);
+        }
+
+        fin.close();
+    }
+
+    vector<vector<int>> adjList(){
+        vector<vector<int>> ret(this->names.size());//ind->list of inds its connected to
+
+        fstream fin;
+
+        fin.open("telangana.csv",ios::in);
+        if(!fin.is_open()) throw runtime_error("Failed to open csv file");
+
+        vector<string> row;
+        string line, word;
+        int i=0;//keeps track of line number which is also the index of this->names
+
+        while(getline(fin, line)){
+            row.clear();
+            stringstream s(line);
+
+            bool first=true;
+            while(getline(s,word,',')){
+                if(first){
+                    first=false;
+                    continue;
+                }
+                row.push_back(word);
+            }
+            for(auto r: row){
+                int ind = find(this->names.begin(), this->names.end(), r) - this->names.begin();
+                ret[i].push_back(ind);
+            }
+            i++;
+        }
+
+        fin.close();
+
+        return ret;
+    }
+};
+
+#endif
